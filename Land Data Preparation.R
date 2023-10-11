433b310675b57ea7430f02b4802ceb90ed63976d
Racheal Wanyoike <97697403+Rachealwanyoike@users.noreply.github.com>
2023-10-11 17:01:53 -0400

    Update Land Data Preparation.R

--- Land Data Preparation.R
+++ Land Data Preparation.R
@@ -1,3 +1,5 @@
+#The grain dataset contains information on land in different mostly Africa, Asia and South America that has been obtained by first world countries for different business purposes. 
+#The code shows how I cleaned and prepared the data for analys
 
 install.packages("tidyverse")
 install.packages("readxl")
